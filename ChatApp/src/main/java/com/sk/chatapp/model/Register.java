ac6bd18fa5fe94eb6f505a99a0591343d305e250
Karthik Vijay <55907622+Karthik-Vijay@users.noreply.github.com>
2020-05-16 21:10:49 +0530

    Create Register.java

---
+++ ChatApp/src/main/java/com/sk/chatapp/model/Register.java
@@ -0,0 +1,27 @@
+package com.sk.chatapp.dao;
+public class Register {
+	
+	private String displayname;
+	 private String username;
+	 private String password;
+	 
+    public String getDisplayname() {
+		return displayname;
+	}
+	public void setDisplayname(String displayname) {
+		this.displayname = displayname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
